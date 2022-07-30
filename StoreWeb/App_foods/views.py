6527d9243522dbe50f78ad2cc5259ee288eacb17
PNYWT <punyawat.suwannatat@hotmail.com>
2022-07-30 15:21:08 +0700

    update
    - Show menu Order
    - Show menu per id Order

--- StoreWeb/App_foods/views.py
+++ StoreWeb/App_foods/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+# List of Menu Foods
+menu_Foods = [
+    {
+        'id': 1, 'title': 'Dark Choco Premium', 'price': 499, 'is_premium': True
+    },
+    {
+        'id': 2, 'title': 'Red Spicy', 'price': 349, 'is_premium': False
+    },
+    {
+        'id': 3, 'title': 'Blue', 'price': 349, 'is_premium': False
+    }
+]
+
 # Create your views here.
 
 def foods(request):
-    return render(request, 'App_foods/Foods.html')
+    context = { 'menuFoods' : menu_Foods}
+    return render(request, 'App_foods/Foods.html', context)
 
 def foodId(request, food_Id):
-    return render(request, 'App_foods/foodId.html', context={'food_Id' : food_Id})
+    idofMune = None
+    try:
+        idofMune = [f for f in menu_Foods if f['id'] == food_Id][0]
+    except IndexError:
+        print("Not Found Index FoodId")
+    context = { 'food' : idofMune}
+    return render(request, 'App_foods/foodId.html', context)
