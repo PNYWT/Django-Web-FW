0e7a40718474e528b92ac4c772da656c185ace0b
PNYWT <punyawat.suwannatat@hotmail.com>
2022-08-02 00:47:50 +0700

    update
    - update Form get and post to model

--- StoreWeb/App_general/views.py
+++ StoreWeb/App_general/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.urls import reverse
 from App_foods.models import FoodModel
+from . models import Subcription
 
 # Create your views here.
 def homePage(request):
@@ -11,6 +13,19 @@ def aboutPage(request):
     return render(request, 'App_general/About.html')
 
 def subcriptionFormPage(request):
+    if request.POST:
+        # print(request.POST)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        food_ids = request.POST.getlist('food_ids')
+        new_sub = Subcription()
+        new_sub.name = name
+        new_sub.email = email
+        # new_sub.save()
+        # print(name)
+        # print(email)
+        # print(food_ids)
+        return HttpResponseRedirect(reverse('Subcription_ty'))
     menu_Foods = FoodModel.objects.order_by('-is_Premium')
     context = { 'menuFoods' : menu_Foods}
     return render(request, 'App_general/Subcription_form.html', context)
