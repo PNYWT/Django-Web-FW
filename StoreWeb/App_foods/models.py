34c7579e46059f907617584ae2b9e3232f7e874a
PNYWT <punyawat.suwannatat@hotmail.com>
2022-08-06 23:06:50 +0700

    update
    - เพิ่ม adminmodel

--- StoreWeb/App_foods/models.py
+++ StoreWeb/App_foods/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
@@ -5,7 +6,10 @@ from django.db import models
 class FoodModel(models.Model):
     title = models.CharField(max_length=60)
     price = models.IntegerField()
-    special_Price = models.IntegerField(null=True)
+    special_Price = models.IntegerField(null=True, blank=True)
     is_Premium = models.BooleanField(default=False)
     end_Promotion = models.DateTimeField(null=True, blank=True)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return '{} (id={})'.format(self.title, self.id)
--- StoreWeb/App_general/models.py
+++ StoreWeb/App_general/models.py
@@ -19,3 +19,6 @@ class Subscription(models.Model):
     status = models.CharField(max_length=15, choices=STATUS_choices, default='unapproved')
     registered_at = models.DateTimeField(auto_now_add=True)
     food_set = models.ManyToManyField('App_foods.FoodModel')
+
+    def __str__(self) -> str:
+        return '{} (id={})'.format(self.name, self.id)
