15f08a4d24ae68134872f714ca655d253071bbd5
PNYWT <punyawat.suwannatat@hotmail.com>
2022-08-21 00:19:45 +0700

    saddas

---
+++ App_foods/models.py
@@ -0,0 +1,16 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+
+class FoodModel(models.Model):
+    title = models.CharField(max_length=60)
+    price = models.IntegerField()
+    special_Price = models.IntegerField(null=True, blank=True)
+    is_Premium = models.BooleanField(default=False)
+    end_Promotion = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    image_relative_url = models.CharField(max_length=60, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return '{} (id={})'.format(self.title, self.id)
