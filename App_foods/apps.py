fa26859ad7de7c5ba6c6e870be26878e728cc239
PNYWT <punyawat.suwannatat@hotmail.com>
2022-08-21 00:17:47 +0700

    asdas

--- App_foods/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppFoodsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'App_foods'
--- App_foods/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import URLPattern, path
-from . import views
-
-
-urlpatterns = [
-    path('', views.foods, name ='Foods'),
-    path('<int:food_Id>', views.foodId, name ='foodId'),
-]
